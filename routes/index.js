59f845151c2529aa19f958335fac39b130788336
eddy <ceo@msoftware.co.kr>
2023-10-31 16:52:51 +0900

    채팅 웹페이지수정

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* 임시메인 */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+//모든 사용자 채팅하기 
+router.get('/chat', function(req, res, next) {
+  res.render('chat.ejs');
+});
+
+
+//채팅방 기준 채팅하기 
+router.get('/groupchat', function(req, res, next) {
+  res.render('groupchat.ejs');
+});
+
+
+//ChatGPT 3.5 터보 사용자 채팅하기 
+router.get('/aichat', function(req, res, next) {
+  res.render('aichat.ejs');
 });
 
 module.exports = router;
